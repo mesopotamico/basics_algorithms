bc096d590a6aeea58cd7d91f4c652055d466a924
Nicolas Duque Gutierrez <n.duque1@utp.edu.co>
2024-09-30 17:11:10 -0500

    Full merge-sort

--- src/mergesort.rs
+++ src/mergesort.rs
@@ -1,27 +1,45 @@
-pub fn mergesort(vector: &mut Vec<i32>) {
-
+pub fn mergesort(vector: &mut Vec<i32>, start: usize , end: usize) {
+    if start < end {
+        let middle: usize = (start + end ) / 2; 
+        mergesort(vector,start, middle);
+        mergesort(vector,middle + 1, end);
+        merge(vector,0, middle, vector.len() -1);
+    }
 }
 
-pub fn merge(vector: &mut Vec<i32>, start:usize, middle: usize, end:usize) {
+pub fn merge(vector: &mut Vec<i32>, start:usize, middle: usize, end:usize){  
+    let mut aux: Vec<i32> = vec![0;vector.len()];
    
+
     let mut index = 0;
-    let mut right_ = 0;
-    let mut left_ = 0;
-    let mut aux: Vec<i32>;
-    while right_ <= middle && left_ <= end{ 
-        if vector[left_] <= vector[right_]{
+    let mut left_ = start;
+    let mut right_ = middle + 1;
+    while left_ <= middle && right_ <= end{ 
+        if vector[left_] <= vector[right_] {
             aux[index] = vector[left_];
             left_ += 1;
+            index += 1;
         }
         if vector[right_] < vector[left_]{
             aux[index] = vector[right_];
             right_ += 1;
+            index += 1;
         }
+    }
+
+
+    for k in right_..=end {
+        aux[index] = vector[k];
         index += 1;
     }
-    if right_ == middle {
-        //Aca hay un problema
 
+    for k in left_..=middle {
+        aux[index] = vector[k];
+        index += 1;
+    }
+
+    for i in 0..vector.len(){
+        vector[i] = aux[i];
     }
 
 }
