d9447929f7b532e46f7c4b26cb745b4d88f95819
Nicolas Duque Gutierrez <n.duque1@utp.edu.co>
2024-10-03 23:07:33 -0500

    Understanding a little bit the borrow checker, not enough

---
+++ src/doublelink.rs
@@ -0,0 +1,38 @@
+struct Node {
+    data: i32,
+    next: Option<Box<Node>>,
+    prev: Option<Box<Node>>,
+}
+
+pub struct DoubleLink {
+    head: Option<Box<Node>>,
+} 
+
+impl DoubleLink {
+    pub fn new() -> Self {
+        DoubleLink {
+            head: None,
+        } 
+    }
+
+    pub fn insert(&mut self,value: i32) {
+        let mut new_node = Box::new(Node{
+            data: value,
+            next: None,
+            prev: None,
+        });
+
+        if self.head.is_none(){
+            self.head = Some(new_node);
+        }
+        else { 
+            let mut old_head = self.head.take().unwrap(); // Tomamos el antiguo cabeza
+            old_head.next = Some(new_node); //Antigua cabeza siguiente es nuevo nodo
+            let mut new_node = old_head.next.take().unwrap(); // Tomamos el antiguo cabeza
+            new_node.prev = Some(old_head); // Nuevo nodo prev apunta a la antigua cabeza
+            self.head = Some(new_node); // Actualizamos la cabeza
+        }
+    }
+}
+
+
--- src/main.rs
+++ src/main.rs
@@ -1,35 +1,19 @@
 mod rand_vec;
 use rand_vec::rand_vec;
 
-mod linkedlist;
-use linkedlist::LinkedList;
+mod doublelink;
+use doublelink::DoubleLink;
+//
+// mod linkedlist;
+// use linkedlist::LinkedList;
 
 
 fn main() {
 
-    // let mut disorder = rand_vec(10);
-    // println!("{:?}", disorder);
-    //
-    // let end: usize = disorder.len() - 1;
-    // mergesort(&mut disorder,0, end);
-    //
-    // println!("Test mergesort: {:?}",disorder);
-    //
-    let mut list = LinkedList::new();
-    list.insert(1);
-    list.insert(2);
-    list.insert(2);
-    list.insert(1);
-    list.insert(5);
-    list.display();
-    let random = rand_vec(10);
-    println!("{:?}", random);
-    match search_element(&random, 7) {
-        None => println!("Index out of bounds"),
-        Some(value) => println!("The value is: {}", value),
+    let mut list = DoubleLink::new();
 
-    }
 }
+
 
 fn search_element(vector: &Vec<i32>, indice: usize) -> Option<&i32> {
     if indice < vector.len(){
@@ -39,3 +23,4 @@ fn search_element(vector: &Vec<i32>, indice: usize) -> Option<&i32> {
         None
     }
 }
+
